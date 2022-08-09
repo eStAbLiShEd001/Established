17607503d0c6ec47cff9bc5c0e5f22db99e019e9
eStAbLiShEd001 <105214437+eStAbLiShEd001@users.noreply.github.com>
2022-08-08 21:14:12 -0700

    made it responsive in all sections

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var myVar;
+// var myVar;
         
-function myFunction() {
-  myVar = setTimeout(showPage, 3000);
-}
+// function myFunction() {
+//   myVar = setTimeout(showPage, 3000);
+// }
 
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
+// function showPage() {
+//   document.getElementById("loader").style.display = "none";
+//   document.getElementById("myDiv").style.display = "block";
+// }
